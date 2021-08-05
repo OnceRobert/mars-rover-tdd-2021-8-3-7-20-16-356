c2370c9b305af4aef9a3c7f984be27d9711774ad
OnceRobert <robpurugganan@gmail.com>
2021-08-05 19:55:15 +0800

    [marsRover][Roberto]feat: implemented code in executeCommand for test case 0 0 W  M

--- src/main/java/com/afs/tdd/marsRover.java
+++ src/main/java/com/afs/tdd/marsRover.java
@@ -19,8 +19,15 @@ public class marsRover {
     
     public roverStatus executeCommand(roverStatus status, String command){
         if(command.equals("M")){
-            int newY = status.getY() + 1;
-            return new roverStatus(status.getX(),newY,status.getDirection());
+            if(status.getDirection().equals("N")){
+                int newY = status.getY() + 1;
+                return new roverStatus(status.getX(),newY,status.getDirection());
+            }else if(status.getDirection().equals("W")){
+                int newX = status.getX() - 1;
+                return new roverStatus(newX, status.getY(), status.getDirection());
+            }
+            else
+                return status;
         }
         else if(command.equals("L")){
             String newDirection = "W";
