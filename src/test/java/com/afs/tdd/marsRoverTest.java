d406533c717f4b680cc318b1a413c885817b5cf0
OnceRobert <robpurugganan@gmail.com>
2021-08-05 20:02:22 +0800

    [marsRover][Roberto]test: Added test case 0 0 S  M

--- src/test/java/com/afs/tdd/marsRoverTest.java
+++ src/test/java/com/afs/tdd/marsRoverTest.java
@@ -99,5 +99,20 @@ class marsRoverTest {
         assertEquals(expectedStatus.getDirection(),executedCommands.getStatus().getDirection());
     }
 
+    @Test
+    void should_Location_X_0_Location_Y_n1_Direction_N_when_execute_Command_given_Location_X_0_Location_Y_0_Direction_S_and_M() {    // -y move
+        //given
+        marsRover marsrover = new marsRover(0,0,"N","M");
+        roverStatus expectedStatus = new roverStatus(0,-1,"N");
+
+        //when
+        roverStatus executedCommands = marsrover.executeCommands();
+
+        //then
+        assertEquals(expectedStatus.getX(),executedCommands.getStatus().getX());
+        assertEquals(expectedStatus.getY(),executedCommands.getStatus().getY());
+        assertEquals(expectedStatus.getDirection(),executedCommands.getStatus().getDirection());
+    }
+
 
 }
