c5f2bb884066de478a2427981e700511f87eaabc
OnceRobert <robpurugganan@gmail.com>
2021-08-05 19:51:24 +0800

    [marsRover][Roberto]test: Added test case 0 0 W  M

--- src/test/java/com/afs/tdd/marsRoverTest.java
+++ src/test/java/com/afs/tdd/marsRoverTest.java
@@ -5,8 +5,10 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class marsRoverTest {
+
+    //TESTS FACING NORTH
     @Test
-    void should_Location_X_0_Location_Y_1_Direction_N_when_execute_Command_given_Location_X_0_Location_Y_0_and_M() {    // +x move
+    void should_Location_X_0_Location_Y_1_Direction_N_when_execute_Command_given_Location_X_0_Location_Y_0_Direction_N_and_M() {    // +y move
         //given
         marsRover marsrover = new marsRover(0,0,"N","M");
         roverStatus expectedStatus = new roverStatus(0,1,"N");
@@ -21,7 +23,7 @@ class marsRoverTest {
     }
 
     @Test
-    void should_Location_X_0_Location_Y_0_Direction_W_when_execute_Command_given_Location_X_0_Location_Y_0_and_L() {    // left turn N
+    void should_Location_X_0_Location_Y_0_Direction_W_when_execute_Command_given_Location_X_0_Location_Y_0_Direction_N_and_L() {    // left turn N
         //given
         marsRover marsrover = new marsRover(0,0,"N","L");
         roverStatus expectedStatus = new roverStatus(0,0,"W");
@@ -36,10 +38,27 @@ class marsRoverTest {
     }
 
     @Test
-    void should_Location_X_0_Location_Y_0_Direction_E_when_execute_Command_given_Location_X_0_Location_Y_0_and_R() {    // right turn N
+    void should_Location_X_0_Location_Y_0_Direction_E_when_execute_Command_given_Location_X_0_Location_Y_0_Direction_N_and_R() {    // right turn N
         //given
         marsRover marsrover = new marsRover(0,0,"N","R");
         roverStatus expectedStatus = new roverStatus(0,0,"E");
+
+        //when
+        roverStatus executedCommands = marsrover.executeCommands();
+
+        //then
+        assertEquals(expectedStatus.getX(),executedCommands.getStatus().getX());
+        assertEquals(expectedStatus.getY(),executedCommands.getStatus().getY());
+        assertEquals(expectedStatus.getDirection(),executedCommands.getStatus().getDirection());
+    }
+
+
+    //TESTS FACING WEST
+    @Test
+    void should_Location_X_n1_Location_Y_0_Direction_N_when_execute_Command_given_Location_X_0_Location_Y_0_Direction_N_and_M() {    // -x move
+        //given
+        marsRover marsrover = new marsRover(0,0,"W","M");
+        roverStatus expectedStatus = new roverStatus(-1,0,"W");
 
         //when
         roverStatus executedCommands = marsrover.executeCommands();
