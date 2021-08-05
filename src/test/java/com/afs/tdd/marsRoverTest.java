e362194201210eb9ef84c1d4155ade1d7392f70a
OnceRobert <robpurugganan@gmail.com>
2021-08-05 19:46:32 +0800

    [marsRover][Roberto]test: added test case 0 0 N  R

--- src/test/java/com/afs/tdd/marsRoverTest.java
+++ src/test/java/com/afs/tdd/marsRoverTest.java
@@ -21,10 +21,25 @@ class marsRoverTest {
     }
 
     @Test
-    void should_Location_X_0_Location_Y_0_Direction_W_when_execute_Command_given_Location_X_0_Location_Y_0_and_L() {    // +x move
+    void should_Location_X_0_Location_Y_0_Direction_W_when_execute_Command_given_Location_X_0_Location_Y_0_and_L() {    // left turn N
         //given
         marsRover marsrover = new marsRover(0,0,"N","L");
         roverStatus expectedStatus = new roverStatus(0,0,"W");
+
+        //when
+        roverStatus executedCommands = marsrover.executeCommands();
+
+        //then
+        assertEquals(expectedStatus.getX(),executedCommands.getStatus().getX());
+        assertEquals(expectedStatus.getY(),executedCommands.getStatus().getY());
+        assertEquals(expectedStatus.getDirection(),executedCommands.getStatus().getDirection());
+    }
+
+    @Test
+    void should_Location_X_0_Location_Y_0_Direction_E_when_execute_Command_given_Location_X_0_Location_Y_0_and_R() {    // right turn N
+        //given
+        marsRover marsrover = new marsRover(0,0,"N","R");
+        roverStatus expectedStatus = new roverStatus(0,0,"E");
 
         //when
         roverStatus executedCommands = marsrover.executeCommands();
